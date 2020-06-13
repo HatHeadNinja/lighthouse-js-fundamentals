8a73284cdf75a4287bf234d623f4d31c6138a38a
Doug Dyson <dougdyson@gmail.com>
2020-06-13 10:47:57 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,12 +1,3 @@
-const raining = true;
-const cold = false;
-
-if (raining) {
-  console.log("Don't forget your umbrella!");
+if (!raining) {
+  console.log("Leave your umbrella at home!");
 }
-
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-}
-
-console.log("Now you're ready to go outside!");
