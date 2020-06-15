9e8fa2453a4f4a022252d5386dabec59d40f1f9c
Doug Dyson <dougdyson@gmail.com>
2020-06-15 11:44:21 +0000

    initial commit

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+// Your code below here...
+iceCreamFlavours.push('root beer');
+
+//original flavours
+console.log(iceCreamFlavours.toString());
+
+//first flavor
+console.log(iceCreamFlavours[0]);
+
+//last flavor
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+//number of flavors
+console.log(iceCreamFlavours.length);
