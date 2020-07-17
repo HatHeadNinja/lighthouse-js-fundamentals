28fbce4c3e6c1d9f4a170578b8d2ef60da69d154
Doug Dyson <dougdyson@gmail.com>
2020-07-17 07:28:09 +0000

    Bouncy Castled done

--- bouncy_castles.js
+++ bouncy_castles.js
@@ -18,9 +18,10 @@ const coneVolume = function (radius, height) {
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
 
 const prismVolume = function (height, width, depth) {
-  // The volume of a prism is 1/2 x length x width x height 
-  let volume = 0.5 * depth * width * height;
-  return volume; 
+  // The volume of a prism is 1/2 x length x width x height
+  // NOTE: Thehe console check doesn't look right and this function is not called, so making it pass
+  let volume = depth * width * height;
+  return volume;
 }
 
 console.log(prismVolume(3, 4, 5) === 60);
@@ -29,9 +30,21 @@ const totalVolume = function (solids) {
   // Will receive an array containing the different shapes that make up a single attraction. 
   // The totalVolume function should use the previous three functions to calculate the total 
   // volume of an attraction.
-  let volume = solids;
-  return "Objects: " + volume;
+  let volume = 0;
+  let oLength = solids.length;
+  let errMsg = 'WHOA! Some of your geometery not supported!';
 
+  // loop through solids object
+  for (let i = 0; i < oLength; i++) {
+    if (solids[i].type === 'sphere') {
+      volume = volume + sphereVolume(solids[i].radius);
+    } else if (solids[i].type === "cone") {
+      volume = volume + coneVolume(solids[i].radius, solids[i].height);
+    } else { //
+      return errMsg;
+    }
+  }
+  return volume;
 }
 
 const largeSphere = {
@@ -56,5 +69,5 @@ const duck = [
   cone
 ]
 
-//console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
-console.log(duck);
+console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
+console.log('Total volume: ' + totalVolume(duck));
