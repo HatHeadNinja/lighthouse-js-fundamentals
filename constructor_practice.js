4013d685c71d97760efe029ad0540fa23d6abf78
Doug Dyson <dougdyson@gmail.com>
2020-07-20 05:52:01 +0000

    organize_instructors.js inital logic, no code

---
+++ constructor_practice.js
@@ -0,0 +1,17 @@
+function CorrectChange(twentyDollar, tenDollar, fiveDollar, twoDollar, oneDollar, quarter, dime, nickel, penny) {
+  /* this.twentyDollar = twentyDollar;
+  this.tenDollar = tenDollar;
+  this.fiveDollar = fiveDollar;
+  this.twoDollar = twoDollar;
+  this.oneDollar = oneDollar;
+  this.quarter = quarter;
+  this.dime = dime;
+  this.nickel = nickel;
+  this.penny = penny; */
+}
+
+let correctChange = new CorrectChange();
+
+correctChange.twentyDollar = 1;
+
+console.log(correctChange);
---
+++ organize_instructors.js
@@ -0,0 +1,53 @@
+/*
+REQUIREMENTS:
+Given a list of instructors and we will create a single object 
+to organize them based on their course.
+
+TEST INPUTS:
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
+]));
+
+EXPECTED OUTPUTS:
+{
+  iOS: ["Samuel"],
+  Web: ["Victoria", "Karim", "Donald"]
+}
+{
+  Blockchain: ["Brendan"],
+  Web: ["David", "Carlos"],
+  iOS: ["Martha"]
+}
+*/
+
+const organizeInstructors = function(instructors) {
+  
+  // Parse courses into new object
+
+
+  // Iterate through new course object and find instructors in array provided by function
+
+  // Return object
+};
+
+console.log(organizeInstructors([
+  {name: "Samuel", course: "iOS"},
+  {name: "Victoria", course: "Web"},
+  {name: "Karim", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(organizeInstructors([
+  {name: "Brendan", course: "Blockchain"},
+  {name: "David", course: "Web"},
+  {name: "Martha", course: "iOS"},
+  {name: "Carlos", course: "Web"}
+]));
